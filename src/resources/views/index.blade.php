16508bb66de89c0e972dfdc80743e2239c11e23f
yukino sakata <yuckeeeey0127@gmail.com>
2023-12-08 23:15:36 +0900

    20231208

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.app')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+    <header class="header">
+        <div class="header__inner">
+            <h1 class="header__title">
+                お問い合わせ
+            </h1>
+        </div>
+    </header>
+
+    <div class="contact-form__content">
+      <form class="form">
+        <!--名前-->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="first_name" value="{{ old('name') }}" />
+                    <input type="text" name="last_name" value="{{ old('name') }}" />
+                </div>
+                <div class="form__error">
+                <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <!--名前-->
+        <!--性別-->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">性別</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--radio">
+                    <input type="radio" name="gender" value="{{ old('gender') }}" />
+                    <label for="men">男性</label>
+                </div>
+                <div class="form__input--radio">
+                    <input type="radio" name="gender" value="{{ old('gender') }}" />
+                    <label for="men">女性</label>
+                </div>
+                <div class="form__error">
+                <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <!--性別-->
+        <!--メールアドれス-->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" value="{{ old('email') }}" />
+                </div>
+                <div class="form__error">
+                <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <!--メールアドれス-->
+        <!--郵便版後-->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">郵便番号</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="postcode" name="postcode" value="{{ old('postcode') }}" />
+                </div>
+                <div class="form__error">
+                <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <!--郵便番号-->
+        <!--住所-->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address" value="{{ old('address') }}" />
+                </div>
+                <div class="form__error">
+                <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <!--住所-->
+        <!--建物名-->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="building_name" value="{{ old('building_name') }}" />
+                </div>
+                <div class="form__error">
+                <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <!--建物名-->
+        <!--ご意見-->
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">ご意見</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--textarea">
+              <textarea name="content" value="{{ old('content') }}"></textarea>
+            </div>
+          </div>
+        </div>
+        <!--ご意見-->
+        <!--確認ボタン-->
+        <div class="form__button">
+          <button class="form__button-submit" type="submit">確認</button>
+        </div>
+        <!--確認ボタン-->
+      </form>
+    </div>
+
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせ</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+    @yield('css')
+</head>
+<body>
+    <main>
+        <div class="content">
+    @yield('content')
+        </div>
+    </main>
+</body>
+</html>
